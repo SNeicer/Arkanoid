58b1f3ea7df86e9cb327af948137b7def8b12bd5
SNeicer <46260745+SNeicer@users.noreply.github.com>
2020-04-11 18:00:29 +0500

    Updated and Added some files

--- Arcanoid.py
+++ Arcanoid.py
@@ -4,10 +4,12 @@ from time import sleep
 pygame.init()
 pygame.font.init()
 
-
+# Настраиваем экран
 size = width, height = (1280, 720)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Basic Arcanoid (Premitive physics)')
+
+# Настраиваем переменные
 IsStart = 0
 Lives = 3
 Score = 0
@@ -15,10 +17,12 @@ Combo = 1
 clock = pygame.time.Clock()
 fps = 30
 
-BounceS = pygame.mixer.Sound('data/Sounds/Bounce.wav')
-GoalS = pygame.mixer.Sound('data/Sounds/Goal.wav')
-LoseS = pygame.mixer.Sound('data/Sounds/Lose.wav')
+# Заносим звуки в переменные
+BounceSNew = pygame.mixer.Sound('data/Sounds/BlockHit.ogg')
+BouncePlayerS = pygame.mixer.Sound('data/Sounds/ArcPlateHit.ogg')
+BrickDestroyS = pygame.mixer.Sound('data/Sounds/BlockHitBreaking.ogg')
 
+# Создаём удобную функцию загрузки спрайтов
 def load_image(name):
     fullname = 'data' + '/' + name
     try:
@@ -32,6 +36,7 @@ def load_image(name):
 
     return image
 
+# Создаём класс платформы
 class Paddle(pygame.sprite.Sprite):
 
     image = load_image('Paddle.png')
@@ -49,6 +54,7 @@ class Paddle(pygame.sprite.Sprite):
     def update(self):
         self.rect = self.rect.move(self.vx, self.vy)
 
+# Создаём класс кирпичика
 class Brick(pygame.sprite.Sprite):
 
     image = load_image('Bricks/Gray.png')
@@ -66,7 +72,7 @@ class Brick(pygame.sprite.Sprite):
         self.rect.y = y
         self.vx = 0
         self.vy = 0
-        self.colorv = random.randint(1, 9) # 1 - 5 - Gray, 6 - Green, 7 - Blue, 8 - Red, 9 - Orange
+        self.colorv = random.randint(1, 9) # 1 - 5 - Gray, 6 - Green, 7 - Blue, 8 - Red, 9 - Orange. Случайно выбираем цвет кирпичика
 
         if self.colorv >= 1 and self.colorv <= 5:
             self.image = Brick.image
@@ -82,27 +88,30 @@ class Brick(pygame.sprite.Sprite):
     def update(self):
         self.rect = self.rect.move(self.vx, self.vy)
 
+        # При соприкосновении мячика с кирпичиком
         if pygame.sprite.spritecollide(self, BallG, False):
             global Combo
             global Score
 
             Score += 5 * Combo
             Combo += 1
-            BounceS.play()
+            BounceSNew.play()
 
             if self.image == Brick.image:
                 BrickBlow(self.rect.x, self.rect.y)
+                BrickDestroyS.play()
                 self.kill()
             else:
                 self.image = Brick.image
 
+# Создаём класс мячика
 class Ball(pygame.sprite.Sprite):
 
     image = load_image('Ball.png')
 
     def __init__(self, x, y):
         super().__init__(all_sprites)
-        self.add(BallG)
+        self.add(BallG) # Добавляем мяч в специальную группу для взаимодействий
         self.image = pygame.transform.scale(Ball.image, (30, 30))
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -113,9 +122,12 @@ class Ball(pygame.sprite.Sprite):
         self.rdirp = 0
 
     def update(self):
-        self.rect = self.rect.move(self.vx, self.vy)
+        self.rect = self.rect.move(self.vx, self.vy) # Двигаем мячик
 
+        # При соприкосновении с кирпичиком меняем направление и проигрываем звук
         if pygame.sprite.spritecollide(self, Bricks, False):
+
+            BounceSNew.play()
 
             self.rdirb = random.randint(1, 3)
             if self.rdirb == 1:
@@ -128,9 +140,10 @@ class Ball(pygame.sprite.Sprite):
                 self.vx = -self.vx
                 self.vy = self.vy
 
+        # Отскакиваем от платформы
         if pygame.sprite.spritecollide(self, P1, False):
             global Combo
-            BounceS.play()
+            BouncePlayerS.play()
             Combo = 1
             self.rdirp = random.randint(1, 2)
 
@@ -141,26 +154,27 @@ class Ball(pygame.sprite.Sprite):
                 self.vx = -self.vx
                 self.vy = -self.vy
 
+        # Отскакиваем от границ экрана
         if self.rect.y <= 0:
             self.vx = self.vx
             self.vy = -self.vy
-            BounceS.play()
 
         if self.rect.x >= 1250 or self.rect.x <= 0:
             self.vx = -self.vx
             self.vy = self.vy
-            BounceS.play()
 
+        # Мячик улител за платформу, теряем одну жизнь
         if self.rect.y >= 720:
             self.foul()
 
-
+    # Функция старта мячика
     def go(self):
         global IsStart
         self.vx = -4
         self.vy = -4
         IsStart = 1
 
+    # Функция для потери жизни
     def foul(self):
         global IsStart
         global Lives
@@ -174,7 +188,6 @@ class Ball(pygame.sprite.Sprite):
             IsStart = 0
             Lives -= 1
             Combo = 1
-            LoseS.play()
         elif Lives <= 0:
             self.vx = 0
             self.vy = 0
@@ -183,6 +196,7 @@ class Ball(pygame.sprite.Sprite):
             IsStart = 2
             Combo = 1
 
+# Создаём класс под задний фон
 class Background(pygame.sprite.Sprite):
 
     image = load_image('ArcBG.png')
@@ -194,6 +208,7 @@ class Background(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
 
+# Создаём класс под анимацию уничтожения кирпичика
 class BrickBlow(pygame.sprite.Sprite):
 
     image = load_image('BrickBreaking/BlockBrake1.png')
@@ -226,22 +241,24 @@ class BrickBlow(pygame.sprite.Sprite):
         if self.frame == 17:
             self.kill()
 
+# Задаём шрифт игры и так-же создаём тексты
 GameFont = pygame.font.SysFont('calibri', 30)
 ScoreText = GameFont.render('Score: ' + str(Score), 1, (255, 255, 255))
 ComboText = GameFont.render('Combo: ' + str(Combo), 1, (255, 255, 255))
 LivesText = GameFont.render('Lives: ' + str(Lives), 1, (255, 255, 255))
 
-
+# Создаём группы взаимодействий
 all_sprites = pygame.sprite.Group()
 P1 = pygame.sprite.Group()
 BallG = pygame.sprite.Group()
 Bricks = pygame.sprite.Group()
 
+# Создаём объекты
 BackGroundObj = Background(0, 0)
 Player = Paddle(540, 640)
 PBall = Ball(628, 550)
 
-
+# Создаём кирпичики
 brk_x, brk_y = 50, 100
 for i in range(7):
     for j in range(9):
@@ -251,9 +268,11 @@ for i in range(7):
     brk_y += 32
     brk_x -= 9 * 128
 
+# Главный цикл игры
 running = True
 while running:
 
+    # Управление персонажем
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_RIGHT] and IsStart == 1 and Player.rect.x < 1080:
@@ -261,13 +280,13 @@ while running:
     if keys[pygame.K_LEFT] and IsStart == 1 and Player.rect.x > 0:
         Player.rect.x -= 6
 
+    # Проверка на уничтожение всех кирпичиков
     if len(Bricks) <= 0:
-        GoalS.play()
         PBall.vx = 0
         PBall.vy = 0
         IsStart = 3
 
-
+    # При нажатии Space вызываем функцию старта у мячика
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and IsStart == 0 and Lives > 0:
             IsStart == 1
@@ -277,6 +296,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # Рендерим (рисуем) все объекты на экран
     screen.fill((0, 0, 0))
 
     ScoreText = GameFont.render('Score: ' + str(Score), 1, (255, 255, 255))
